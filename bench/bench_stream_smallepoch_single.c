6a14e23b9e2c244c509855fc0f95037b324d2b57
Nathan Clack <nclack@gmail.com>
2026-04-18 12:57:30 -0700

    smallepoch: 16M frames, 64K shard

--- bench/bench_stream_smallepoch_single.c
+++ bench/bench_stream_smallepoch_single.c
@@ -5,7 +5,7 @@ int
 main(int ac, char* av[])
 {
   struct dimension dims[3];
-  uint64_t sizes[] = { 65536, 16, 16 };
+  uint64_t sizes[] = { 1 << 24, 16, 16 };
   uint8_t rank = dims_create(dims, "tyx", sizes);
 
   uint8_t ratios[] = { 1, 0, 0 };
@@ -18,8 +18,8 @@ main(int ac, char* av[])
                              .rank = rank,
                              .chunk_ratios = ratios,
                              .default_chunk_bytes = 1 << 16,
-                             .min_chunk_bytes = 1 << 16,
-                             .min_shard_bytes = 1ull << 30,
+                             .min_chunk_bytes = 1 << 12,
+                             .min_shard_bytes = 1 << 16,
                              .max_concurrent_shards = 1,
                            });
 }
